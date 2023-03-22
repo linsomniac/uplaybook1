fd5a28d71b163f5215fe72c09f0acf9a52afa881
Sean Reifschneider <sean@realgo.com>
2023-03-21 21:38:24 -0600

    Enhancing the test module importing

--- tests/test_up_loop.py
+++ tests/test_up_loop.py
@@ -1,27 +1,70 @@
 #!/usr/bin/env python3
 
-import sys
-from pathlib import Path
-import os
-import types
+from typing import List
+from types import ModuleType
 
-for up_try in ['up', '../up']:
-    if os.path.exists(up_try):
-        up_found = Path(up_try).resolve()
-        break
-else:
-    raise RuntimeError("Unable to find 'up' module to import")
 
-with open(up_found, 'r') as fp:
-    module_bytecode = compile(fp.read(), up_found.parent, 'exec')
-module = types.ModuleType('up')
-exec(module_bytecode, module.__dict__)
-sys.modules['up'] = module
+def import_script_as_module(module_name: str, paths_to_try: List[str]) -> ModuleType:
+    """
+    Imports a Python script as a module, whether it ends in ".py" or not.
+    Given the name of a module to import, and a list of absolute or relative path names
+    (including the filename), import the module.  The module is set up so that it can
+    later be imported, but a reference to the module is also returned.
 
-import up
+    Args:
+        module_name (str): The name of the module to import.
+                This doesn't have to match the filename.
+        paths_to_try (List[str]): A list of file paths to look for the file to load.
+                This can be absolute or relative paths to the file, the first file that
+                exists is used.
+
+    Returns:
+        Module: A reference to the imported module.
+
+    Raises:
+        FileNotFoundError: If the module file is not found in any of the specified directory paths.
+        ImportError: If there are issues importing the module, such as invalid Python syntax in the module file.
+
+    Example:
+        my_module = import_script_as_module("my_module", ["my_module", "../my_module"])
+
+        # Now you can either directly use "my_module"
+        my_module.function()
+
+        # Or you can later import it:
+        import my_module
+    """
+    from pathlib import Path
+    import os
+
+    for try_filename in paths_to_try:
+        if os.path.exists(try_filename):
+            module_filename = Path(try_filename).resolve()
+            break
+    else:
+        raise FileNotFoundError(f"Unable to find '{module_name}' module to import")
+
+    from importlib.util import spec_from_loader, module_from_spec
+    from importlib.machinery import SourceFileLoader
+    import sys
+
+    spec = spec_from_loader(
+        module_name, SourceFileLoader(module_name, str(module_filename))
+    )
+    if spec is None:
+        raise ImportError("Unable to spec_from_loader() the module, no error returned.")
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    sys.modules["up"] = module
+
+    return module
+
+
+up = import_script_as_module("up", ["./up", "../up"])
 
 import unittest
 from collections import OrderedDict
+
 
 class TestUnrollLoops(unittest.TestCase):
     def test_no_loop(self):
@@ -35,7 +78,9 @@ class TestUnrollLoops(unittest.TestCase):
         self.assertEqual(up.unroll_loops(input_list), expected_output)
 
     def test_loop_override(self):
-        input_list = [OrderedDict(a=1, b=2, loop=[OrderedDict(c=3), OrderedDict(b=5, d=4)])]
+        input_list = [
+            OrderedDict(a=1, b=2, loop=[OrderedDict(c=3), OrderedDict(b=5, d=4)])
+        ]
         expected_output = [OrderedDict(a=1, b=2, c=3), OrderedDict(a=1, b=5, d=4)]
         self.assertEqual(up.unroll_loops(input_list), expected_output)
 
@@ -43,6 +88,7 @@ class TestUnrollLoops(unittest.TestCase):
         input_list = []
         expected_output = []
         self.assertEqual(up.unroll_loops(input_list), expected_output)
+
 
 if __name__ == "__main__":
     unittest.main()
